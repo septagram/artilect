246b45f4a88cb5c3ea269d52620fb1ce7a5c786a
Igor Novikov <septagramm@gmail.com>
2025-12-23 22:50:42 +0400

    separate base_url from prefix (prefix goes to HttpClient, also is_full_url), move enum part into CurrentCookies; end up laying foundations for private key token refresh

--- src/precept/client/http.rs
+++ src/precept/client/http.rs
@@ -6,37 +6,106 @@ use itertools::Itertools;
 use keyring::Entry;
 use parking_lot::Mutex;
 use reqwest::header::HeaderValue;
-use serde::{Deserialize, Serialize, Serializer};
-use serde::de::DeserializeOwned;
-use time::{UtcDateTime, UtcOffset};
+use serde::{Deserialize, Serialize, Serializer, de::DeserializeOwned};
+use time::{Duration, OffsetDateTime, UtcDateTime, UtcOffset};
 use tokio::sync;
 use url::Url;
 
 #[cfg(feature = "backend")]
 use crate::auth::middleware::JwtClaims;
-use crate::{auth::dto::RefreshTokenApiResponse, precept, precept::{Identity, IntoPreceptResult, IntoPreceptSpecificResultTyped, PreceptID}, util::report_err::*};
+use crate::{
+    auth::dto::RefreshTokenApiResponse,
+    precept,
+    precept::{Identity, IntoPreceptResult, IntoPreceptSpecificResultTyped, PreceptID},
+    util::report_err::*,
+};
 // @note: All of the below should've been like one line of code. Seriously. It's 2025.
 
 pub const KEYRING_SERVICE_NAME: &str = "artilect";
 
-#[derive(Debug, Default, Serialize, Deserialize)]
-struct CurrentCookies {
-    cookies: cookie_store::CookieStore,
-    #[serde(serialize_with = "serialize_expiration")]
-    access_token_exp: Option<CookieExpiration>,
-    #[serde(serialize_with = "serialize_expiration")]
-    refresh_token_exp: Option<CookieExpiration>,
+struct HeaderExpPair {
+    header: HeaderValue,
+    exp: CookieExpiration,
 }
 
-pub enum SecretProvider {
+enum ClientSecrets {
     #[cfg(feature = "backend")]
-    PreceptCookie { cookie_header: HeaderValue },
+    PreceptCookie {
+        precept_id: PreceptID,
+        exp_duration: Duration,
+        cookie_header: Option<HeaderExpPair>,
+    },
     #[cfg(any(feature = "desktop", feature = "mobile"))]
     UserCookiesNative {
         artilect_base_url: Url,
-        current_cookies: Mutex<CurrentCookies>,
-        warnings_tx: sync::mpsc::Sender<anyhow::Error>,
+        refresh_token_url: Url,
+        cookies: cookie_store::CookieStore,
+        access_token_exp: Option<CookieExpiration>,
+        // refresh_token_exp: Option<CookieExpiration>,
     },
+}
+
+#[derive(Serialize, Deserialize)]
+#[cfg(any(feature = "desktop", feature = "mobile"))]
+enum KeyringSecretStorage {
+    Cookies {
+        cookies: cookie_store::CookieStore,
+        #[serde(serialize_with = "serialize_expiration")]
+        access_token_exp: Option<CookieExpiration>,
+        #[serde(serialize_with = "serialize_expiration")]
+        refresh_token_exp: Option<CookieExpiration>,
+    },
+}
+
+impl ClientSecrets {
+    #[cfg(feature = "backend")]
+    fn default_precept(precept_id: PreceptID, exp_duration: Duration) -> Self {
+        Self::PreceptCookie {
+            precept_id,
+            exp_duration,
+            cookie_header: None,
+        }
+    }
+
+    #[cfg(any(feature = "desktop", feature = "mobile"))]
+    fn default_user(artilect_base_url: Url, refresh_token_url: Url) -> Self {
+        Self::UserCookiesNative {
+            artilect_base_url,
+            refresh_token_url,
+            cookies: cookie_store::CookieStore::new(),
+            access_token_exp: None,
+            refresh_token_exp: None,
+        }
+    }
+
+    #[cfg(any(feature = "desktop", feature = "mobile"))]
+    fn load(artilect_base_url: Url, refresh_token_url: Url) -> anyhow::Result<Self> {
+        match Entry::new(KEYRING_SERVICE_NAME, &*artilect_base_url)
+            .and_then(|entry| entry.get_secret())
+        {
+            Ok(secret) => match serde_json::from_slice::<KeyringSecretStorage>(&*secret)? {
+                #[cfg(any(feature = "desktop", feature = "mobile"))]
+                KeyringSecretStorage::Cookies {
+                    cookies,
+                    access_token_exp,
+                    refresh_token_exp,
+                } => Ok(Self::UserCookiesNative {
+                    artilect_base_url,
+                    refresh_token_url,
+                    cookies,
+                    access_token_exp,
+                    refresh_token_exp,
+                }),
+            },
+            Err(keyring::Error::NoEntry) => Ok(Self::default_user(artilect_base_url, refresh_token_url)),
+            Err(err) => Err(anyhow::anyhow!(err)),
+        }
+    }
+}
+
+pub struct SecretProvider {
+    secrets: Mutex<ClientSecrets>,
+    warnings_tx: sync::mpsc::Sender<anyhow::Error>,
 }
 
 impl PartialEq for SecretProvider {
@@ -49,24 +118,21 @@ impl Eq for SecretProvider {}
 
 impl reqwest::cookie::CookieStore for SecretProvider {
     fn set_cookies(&self, cookie_headers: &mut dyn Iterator<Item = &HeaderValue>, url: &Url) {
-        match self {
+        let warnings_tx = &self.warnings_tx;
+        match self.secrets.lock() {
             #[cfg(feature = "backend")]
-            Self::PreceptCookie { .. } => {} // Cookies cannot be modified for precepts.
+            ClientSecrets::PreceptCookie { .. } => {} // Cookies cannot be modified for precepts.
             #[cfg(any(feature = "desktop", feature = "mobile"))]
-            Self::UserCookiesNative {
-                artilect_base_url,
-                current_cookies,
-                warnings_tx,
+            ClientSecrets::UserCookiesNative {
+                ref artilect_base_url,
+                ref mut cookies,
+                ref mut access_token_exp,
+                ref mut refresh_token_exp,
+                ..
             } => {
                 let parse_cookie_err = || format!("Invalid cookie header for {url}");
                 let add_cookie_err = || format!("Failed to add cookie for {url}");
                 let save_cookie_err = || format!("Failed to save cookies for {url}");
-                let mut lock = current_cookies.lock();
-                let CurrentCookies {
-                    ref mut cookies,
-                    ref mut access_token_exp,
-                    ref mut refresh_token_exp,
-                } = *lock;
                 let mut has_changes = false;
                 for cookie_header in cookie_headers {
                     let cookie: Result<cookie_store::Cookie, anyhow::Error> = try {
@@ -112,18 +178,44 @@ impl reqwest::cookie::CookieStore for SecretProvider {
     }
 
     fn cookies(&self, url: &Url) -> Option<HeaderValue> {
-        match self {
+        match self.secrets.lock() {
             #[cfg(feature = "backend")]
-            Self::PreceptCookie { cookie_header } => Some(cookie_header.clone()),
+            ClientSecrets::PreceptCookie {
+                ref precept_id,
+                ref exp_duration,
+                ref mut cookie_header,
+            } => match cookie_header {
+                Some(HeaderExpPair {
+                    ref header,
+                    ref exp,
+                }) if !exp.is_expired() => Some(header.clone()),
+                _ => {
+                    let exp = OffsetDateTime::now_utc() + exp_duration;
+                    let header: Result<HeaderValue, anyhow::Error> = try {
+                        use crate::auth::middleware::{JwtClaims, JwtClaimsAccess};
+                        let token = JwtClaimsAccess::new(precept_id.into(), exp)
+                            .to_token()
+                            .context("Failed to generate precept access token")
+                            .ok_or_report(&self.warnings_tx)?;
+                        let cookie_header_str =
+                            cookie::Cookie::new("at", &*token.token).to_string();
+                        let mut cookie_header = HeaderValue::try_from(cookie_header_str)
+                            .context("Failed to construct precept access token cookie header")?;
+                        cookie_header.set_sensitive(true);
+                        Ok(cookie_header)
+                    };
+                    header.ok_or_report(&self.warnings_tx).map(|header| {
+                        *cookie_header = Some(HeaderExpPair {
+                            header: header.clone(),
+                            exp: exp.into(),
+                        });
+                        header
+                    })
+                }
+            },
             #[cfg(any(feature = "desktop", feature = "mobile"))]
-            Self::UserCookiesNative {
-                artilect_base_url: _,
-                current_cookies,
-                warnings_tx,
-            } => {
-                let all_cookies = current_cookies
-                    .lock()
-                    .cookies
+            ClientSecrets::UserCookiesNative { ref cookies, .. } => {
+                let all_cookies = cookies
                     .get_request_values(url)
                     .map(|(name, value)| cookie::Cookie::new(name, value).to_string())
                     .join("; ");
@@ -131,7 +223,7 @@ impl reqwest::cookie::CookieStore for SecretProvider {
                     true => None,
                     false => HeaderValue::try_from(all_cookies)
                         .with_context(|| format!("Failed to construct cookie header for {url}"))
-                        .ok_or_report(warnings_tx)
+                        .ok_or_report(self.warnings_tx)
                         .map(|mut header_value| {
                             header_value.set_sensitive(true);
                             header_value
@@ -145,7 +237,6 @@ impl reqwest::cookie::CookieStore for SecretProvider {
 impl SecretProvider {
     #[cfg(feature = "backend")]
     pub fn new_precept(id: PreceptID) -> Result<Self, anyhow::Error> {
-        use crate::auth::middleware::{JwtClaims, JwtClaimsAccess};
         let token = JwtClaimsAccess::new(
             Identity::Precept {
                 id,
@@ -165,17 +256,28 @@ impl SecretProvider {
 
     #[cfg(any(feature = "desktop", feature = "mobile"))]
     pub fn new_user(
-        artilect_base_url: Arc<str>,
+        artilect_base_url: Url,
+        warnings_tx: sync::mpsc::Sender<anyhow::Error>,
     ) -> Result<(Self, sync::mpsc::Receiver<anyhow::Error>), anyhow::Error> {
-        let (warnings_tx, warnings_rx) = sync::mpsc::channel(32);
-        let current_cookies = get_current_cookies(&*artilect_base_url)
+        let secrets = get_current_cookies(&*artilect_base_url)
             .ok_or_report(&warnings_tx)
-            .unwrap_or_default();
+            .unwrap_or_else(|| ClientSecrets::UserCookiesNative {
+                cookies: cookie_store::CookieStore::new(),
+                access_token_exp: None,
+                refresh_token_exp: None,
+            });
+        let artilect_base_url = Url::parse(&*artilect_base_url)
+            .with_context(|| format!("Invalid URL: {}", artilect_base_url))?;
         Ok((
-            Self::UserCookiesNative {
-                artilect_base_url: Url::parse(&*artilect_base_url)
-                    .with_context(|| format!("Invalid URL: {}", artilect_base_url))?,
-                current_cookies: Mutex::new(current_cookies),
+            Self {
+                artilect_base_url,
+                refresh_token_url: artilect_base_url.join("auth/refresh").with_context(|| {
+                    format!(
+                        "Failed to generate refresh token URL for {}",
+                        artilect_base_url
+                    )
+                })?,
+                secrets: Mutex::new(secrets),
                 warnings_tx,
             },
             warnings_rx,
@@ -187,7 +289,10 @@ impl SecretProvider {
             #[cfg(feature = "backend")]
             Self::PreceptCookie { .. } => false,
             #[cfg(any(feature = "desktop", feature = "mobile"))]
-            Self::UserCookiesNative { current_cookies, .. } => {
+            Self::UserCookiesNative {
+                secrets: current_cookies,
+                ..
+            } => {
                 let lock = current_cookies.lock();
                 let access_expired = match &lock.access_token_exp {
                     None => true,
@@ -203,11 +308,11 @@ impl SecretProvider {
     }
 }
 
-fn get_current_cookies(artilect_base_url: &str) -> anyhow::Result<CurrentCookies> {
+fn get_current_cookies(artilect_base_url: &str) -> anyhow::Result<ClientSecrets> {
     match Entry::new(KEYRING_SERVICE_NAME, &*artilect_base_url).and_then(|entry| entry.get_secret())
     {
-        Ok(secret) => Ok(serde_json::from_slice::<CurrentCookies>(&*secret)?),
-        Err(keyring::Error::NoEntry) => Ok(CurrentCookies::default()),
+        Ok(secret) => Ok(serde_json::from_slice::<ClientSecrets>(&*secret)?),
+        Err(keyring::Error::NoEntry) => Ok(ClientSecrets::default()),
         Err(err) => Err(anyhow::anyhow!(err)),
     }
 }
@@ -266,8 +371,13 @@ impl HttpClient {
             self.refresh_token().await?;
         }
         let request = msg.into_request(&self.client, self.base_url.as_ref());
-        request.send().await.into_precept_result_t::<S::Response>().await
+        request
+            .send()
+            .await
+            .into_precept_result_t::<S::Response>()
+            .await
     }
+    // @todo: consume login/refresh payload
 }
 
 impl PartialEq for HttpClient {
